d448ce190d4e4bffbc1ef9afdba7a03dc1848992
AmarRajanMishra <shrutiagarwal493@gmail.com>
2023-08-29 19:05:49 +0530

    updated

--- client/src/components/user/PaymentForm.jsx
+++ client/src/components/user/PaymentForm.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import DropIn from "braintree-web-drop-in-react";
 
-function PaymentForm() {
+function PaymentForm({
+  onClose,
+  clientToken,
+  auth,
+  instance,
+  loading,
+  cart,
+  setInstance,
+  handlePayment,
+}) {
   return (
-    <div>PaymentForm</div>
-  )
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <button
+          className="absolute top-2 right-2 text-gray-500 cursor-pointer"
+          onClick={onClose}
+        >
+          X
+        </button>
+        {/* DropIn component */}
+        {
+          clientToken && auth?.token && cart?.length && (
+            <>
+              <DropIn
+              options={{
+                authorization : clientToken,
+                paypal : {
+                  flow : "vault"
+                }
+              }}
+              onInstance={(instance)=> setInstance(instance)}
+              />
+              <button 
+              onClick={handlePayment}
+              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md disabled:opacity-50">
+                {loading ? "Proccessing...": "Make Payment"}
+              </button>
+            </>
+          )
+        }
+        
+      </div>
+    </section>
+  );
 }
 
-export default PaymentForm
+export default PaymentForm;
--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -5,6 +5,7 @@ import { useAuth } from '../components/context/auth'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
+import PaymentForm from "../components/user/PaymentForm";
 
 function CartPage() {
     const navigate = useNavigate()
@@ -191,6 +192,33 @@ function CartPage() {
                       >
                         Checkout
                       </Link>
+                      <div>
+                  {isPopupOpen && (
+                    <div className="fixed inset-0 flex items-center justify-center z-10 bg-black bg-opacity-50">
+                      <div className="bg-white p-6 rounded-md shadow-lg max-w-md w-full mt-10">
+                        <button
+                          onClick={closePopup}
+                          className="top-2 right-2 text-gray-500 cursor-pointer"
+                        >
+                          X
+                        </button>
+                        <h2 className="text-xl font-semibold mb-4">
+                          Payment Details
+                        </h2>
+                        <PaymentForm 
+                        onClose={closePopup}
+                        clientToken={clientToken}
+                        auth={auth}
+                        instance={instance}
+                        loading={loading}
+                        cart={cart}
+                        setInstance={setInstance}
+                        handlePayment={handlePayment}
+                        />
+                      </div>
+                    </div>
+                  )}
+                </div>
                     </div>
                     
                     <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
